f518347c018a4ecdb96cc0adb98a4edbd5c2f124
ASA <karoteglathe@gmail.com>
2017-04-26 17:15:25 +0800

    fix devServer.js

--- devServer.js
+++ devServer.js
@@ -3,10 +3,11 @@
 const path = require('path');
 const express = require('express');
 const config = require('./webpack.config.js');
+const resolve = file => path.resolve(__dirname, file);
+const isProd = process.env.NODE_ENV === 'production';
 
 const port = 8081;
 const app = express();
-const serve = (path, cache) => express.static(__dirname + path);
 
 const webpack = require('webpack');
 const webpackMiddleware = require('webpack-dev-middleware');
@@ -27,12 +28,12 @@ const middleware = webpackMiddleware(compiler, {
 
 const serve = (path, cache) => express.static(resolve(path), {
   maxAge: cache && isProd ? 60 * 60 * 24 * 30 : 0
-})
+});
 
-app.use('/dist', serve('./dist', true))
-app.use('/public', serve('./public', true))
-app.use('/manifest.json', serve('./manifest.json', true))
-app.use('/service-worker.js', serve('./dist/service-worker.js'))
+app.use('/dist', serve('./dist', true));
+app.use('/public', serve('./public', true));
+app.use('/manifest.json', serve('./manifest.json', true));
+app.use('/service-worker.js', serve('./dist/service-worker.js'));
 
 app.use(middleware);
 app.use(webpackHotMiddleware(compiler));
@@ -40,7 +41,7 @@ app.get('*', function response(req, res) {
 	res.sendFile(path.join(__dirname, '/dist/index.html'));
 });
 
-const server = app.listen(port, 'localhost', function onStart(err) {
+app.listen(port, 'localhost', function onStart(err) {
   if (err) {
     console.log(err);
   }
